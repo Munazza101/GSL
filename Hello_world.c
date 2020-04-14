d11511203326a48af224f00084f5ee2647c04361
Obaid Ur Rehman <16-se-51@students.uettaxila.edu.pk>
2020-04-14 23:21:50 +0500

    Update Hello_world.c

--- Hello_world.c
+++ Hello_world.c
@@ -6,6 +6,8 @@ main (void)
 {
 double x = 5.0;
 double y = gsl_sf_bessel_J0 (x);
+double z= x* gsl/gsl_sf_bessel j(3);
+printf ("int value " \namespace)
 printf (“J0(%g) = %.18e\n”, x, y);
 return 0;
 }
